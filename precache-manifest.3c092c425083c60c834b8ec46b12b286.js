dd8cec9e1af9870df11cfa09d0055a365c93e99f
don-30 <donababu2002@gmail.com>
2024-11-02 00:30:08 +0530

    Updates

---
+++ precache-manifest.3c092c425083c60c834b8ec46b12b286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60873bebb99615fa9fcf6e1d1bb8154",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9254bd07f393ec4ea9ec",
+    "url": "/my-app/static/css/main.e3489625.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/my-app/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/my-app/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "9254bd07f393ec4ea9ec",
+    "url": "/my-app/static/js/main.83cd96ac.chunk.js"
+  },
+  {
+    "revision": "330a69b442416f742e8c",
+    "url": "/my-app/static/js/runtime~main.148f857f.js"
+  }
+]);
